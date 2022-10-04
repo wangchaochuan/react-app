183fe6497f1aaf997b2896984c5ad5a74afa78f2
wangchaochuan <wangcci@enn.cn>
2022-10-04 21:37:04 +0800

    feat(routes/home):测试第三方工具库wangcc-utils

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 import { Button } from 'antd';
 import styles from './index.module.scss';
 import { useNavigate } from 'react-router-dom';
+import { dayOfYear } from 'wangcc-utils';
 
 const Home: FC = () => {
   const navigate = useNavigate();
   const jump = () => {
     navigate('/product');
   };
+  console.log(dayOfYear());
   return (
     <div className={styles.home}>
       <div className={styles.header}>react app home</div>
